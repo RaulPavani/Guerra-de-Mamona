027e6f4cfb8595049813bb942129c760fa6a60f1
Raul Pavani <raul.pavani@hotmail.com>
2024-01-15 14:55:14 -0300

    Project Imports

---
+++ GuerraDeMamona/Assets/Scripts/Singleton.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    private static T instance = null;
+    protected virtual bool dontDestroyOnLoad { get; } = false;
+    protected virtual bool destroyNewInstances { get; } = false;
+
+    public static T Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    public static TYPE InstanceAs<TYPE>() where TYPE : T { return instance as TYPE; }
+
+    public static void IfInstanceNotNull(System.Action<T> onNotNull = null, System.Action onNull = null)
+    {
+        if (instance != null)
+        {
+            onNotNull?.Invoke(instance);
+        }
+        else
+        {
+            onNull?.Invoke();
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (instance != null)
+        {
+            if (destroyNewInstances)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+        else
+        {
+            instance = (T)this;
+            if (dontDestroyOnLoad) { DontDestroyOnLoad(this); }
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (this == instance)
+        {
+            instance = null;
+        }
+    }
+}
